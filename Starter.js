ffbc674fcb0f7acbfe66b3d4738fba7f19f0d6d5
Olivia-Codes <fostero@mail.gvsu.edu>
2025-04-02 09:48:14 -0400

    Update Starter.js

--- Starter.js
+++ Starter.js
@@ -1,39 +1,101 @@
+const readline = require('readline');
 
-const url ='https://gutendex.com/books?search=';
-//const urlID = `https://www.gutenberg.org/ebooks/${bookId}.txt.utf-8`
-async function getData(str){
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    //Bodies with all the headers We just wanna know comm done
-    //We wanna pull out json data
+const url = 'https://gutendex.com/books?search=';
 
-    const request = await fetch(url + str);
-    const json =await request.json();//Consume promise using await
-    console.log(json) //Between 0 and 32 objects(books)
-
-    //Count has how many objects being returned.
-    //results is the key and the value is an array that holds 21 objects.
-    // json.formats
-    //https://www.gutenberg.org/ebooks/1661.txt.utf-8
-    for(let i = 0; i<json.count; ++i){
-
-        console.log(json.results[i].title);
-    }
-    bookId = json.results[0].id;
-    const Newrequest = await fetch(`https://www.gutenberg.org/ebooks/${bookId}.txt.utf-8`);
-    const Newjson= await Newrequest.text();
-    console.log(Newjson)
-
+// askQuestion from class
+function askQuestion(query) {
+    return new Promise(resolve => {
+        rl.question(query, answer => {
+            resolve(answer);
+        });
+    });
 }
-//function askQuestion(query){
-//	return new Promise(resolve => {
-//		rl.question(query, answer => {
-//			resolve(answer);
-//		});
-//	});
-//}
 
-//async function getBook(){
-//	const answer = await askQuestion("Give me an author or book title you want! ");
+// Getting book
+async function getBook(str) {
+    const request = await fetch(url + str);
+    const json = await request.json(); // Consume promise using await
+    console.log(`Found ${json.count} books.`); // How many books we got back
+
+    if (json.count > 0) {
+        // List the titles and authors
+        const bookTitles = json.results.map((book, index) => {
+            console.log(`${index + 1}. Title: ${book.title}, Author: ${book.authors.map(a => a.name).join(', ')}`);
+            return book;
+        });
+
+        // what book you want
+        const choice = await askQuestion(`Please enter the number of the book you'd like to select (1-${json.count}): `);
+        const selectedBook = json.results[parseInt(choice) - 1]; // grab book using index
+
+        if (selectedBook) {
+            console.log(`\nYou selected: ${selectedBook.title} by ${selectedBook.authors.map(a => a.name).join(', ')}`);
+
+            // I'm getting the text of the book
+            const bookId = selectedBook.id;
+            const newRequest = await fetch(`https://www.gutenberg.org/ebooks/${bookId}.txt.utf-8`);
+            const newJson = await newRequest.text();
+            //console.log(newJson);
+            //Creating an array called pages and saving by character, we can easily make this a dictionary
+            const pages = [];
+            for (let i = 0; i < newJson.length; i += 2000) {
+                pages.push(newJson.substring(i, i + 2000));
+            }
 
 
-getData("Adventures of Sherlock Holmes: Illustrated")
+            //console.log(pages);
+            let currentPage = 0;
+            console.log(`\n---\nBook Preview (Page 1):\n`);
+            console.log(pages[currentPage]); // Show the first page
+
+            // go through pages starting at 0
+            let navigating = true;
+            while (navigating) {
+                const action = await askQuestion("\nType 'next' for next page, 'prev' for previous page, or 'quit' to exit: ");
+
+                if (action.toLowerCase() === 'next' && currentPage < pages.length - 1) {
+                    currentPage++;
+                    console.log(`\n---\nBook Preview (Page ${currentPage + 1}):\n`);
+                    console.log(pages[currentPage]);
+                } else if (action.toLowerCase() === 'prev' && currentPage > 0) {
+                    currentPage--;
+                    console.log(`\n---\nBook Preview (Page ${currentPage + 1}):\n`);
+                    console.log(pages[currentPage]);
+                } else if (action.toLowerCase() === 'quit') {
+                    navigating = false;
+                    console.log('Exiting.');
+                } else {
+                    console.log("Invalid input. Please type 'next', 'prev', or 'quit'.");
+                }
+            }
+        } else {
+            console.log('Invalid choice. Please try again.');
+        }
+    } else {
+        console.log('No books found.');
+    }
+}
+
+// Driver for now
+async function bookSearch() {
+    const searchBy = await askQuestion("Would you like to search by 'title' or 'author'? ");
+
+    if (searchBy.toLowerCase() !== 'title' && searchBy.toLowerCase() !== 'author') {
+        console.log("Invalid input, please choose either 'title' or 'author'.");
+        return bookSearch(); // Ask again if the input is bad
+    }
+
+    const searchTerm = await askQuestion(`Please enter the ${searchBy} you want to search for: `);
+
+    console.log(`Searching for books by ${searchBy}...`);
+    await getBook(searchTerm);
+
+    rl.close(); // Close the readline interface
+}
+
+bookSearch(); //Call that
